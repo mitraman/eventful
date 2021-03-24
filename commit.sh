a4b1e5c18fa0c49970e93490dec4995a1f9b6e62
mamund <mamund@yahoo.com>
2021-03-23 20:37:29 -0400

    update repo

---
+++ commit.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# update remote git repo
+
+if [ -z "$1" ] 
+then
+  echo USAGE: commit "'comment'"
+else
+  git add --all
+  git status
+  git commit -m"$1"
+  git push origin master
+fi
+
+# eof
+
+
