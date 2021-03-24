88d4b66b3fc5b2b445dafbe103bba48590e14b5f
mamund <mamund@yahoo.com>
2021-03-23 20:57:04 -0400

    update preface

--- push.sh
+++ push.sh
@@ -6,15 +6,10 @@ if [ -z "$1" ]
 then
   echo USAGE: push "'comment'"
 else
-  if [ -z "$git status --porcelain)" ]
-  then
-    echo "no changes found."
-  else  
-    git add --all
-    git status
-    git commit -m"$1"
-    git push origin master
-  fi  
+  git add --all
+  git status
+  git commit -m"$1"
+  git push origin master
 fi
 
 # eof
